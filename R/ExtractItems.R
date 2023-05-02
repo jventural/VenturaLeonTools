dd1917eb133324683878d08b9060f0978618acaa
jventural <jventuraleon@gmail.com>
2023-05-02 09:51:14 -0500

    Extract_items mod

--- R/ExtractItems.R
+++ R/ExtractItems.R
@@ -1,10 +1,10 @@
-extract_items <- function(text) {
+extract_items <- function(text, prefix = "E") {
   lines <- strsplit(text, "\n")[[1]]
   extracted <- list()
   for (line in lines) {
     words <- strsplit(line, ": ")[[1]][-1]
     tag <- strsplit(line, ": ")[[1]][1]
-    values <- paste0("E", gsub("E", "", unlist(strsplit(words, ", "))))
+    values <- paste0(prefix, gsub(prefix, "", unlist(strsplit(words, ", "))))
     extracted[[tag]] <- values
   }
   return(extracted)
