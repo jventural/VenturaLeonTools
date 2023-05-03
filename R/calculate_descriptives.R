a05ec8f63333766c336e5c19045934931b863023
jventural <jventuraleon@gmail.com>
2023-05-03 06:32:03 -0500

    new functions

---
+++ R/calculate_descriptives.R
@@ -0,0 +1,10 @@
+calculate_descriptives <- function(data, start_col, end_col) {
+  data %>%
+    select(start_col:end_col) %>%
+    psych::describe() %>%
+    as.data.frame() %>%
+    mutate("%" = mean/max*100) %>%
+    select(mean, sd, min, max, skew, kurtosis, "%") %>%
+    rename(Media = mean, DE = sd, Min. = min, Max. = max, g1 = skew, g2 = kurtosis) %>%
+    round(2)
+}
