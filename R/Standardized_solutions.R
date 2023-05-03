82959bd33ce70f1a4c2243ba1116ff6bd13e3b41
jventural <jventuraleon@gmail.com>
2023-05-03 12:59:02 -0500

    Standardized_solutions mod

--- R/Standardized_solutions.R
+++ R/Standardized_solutions.R
@@ -3,8 +3,6 @@ Standardized_solutions <- function(specification, name_items, apply_threshold = 
   library(dplyr)
   library(stringr)
 
-  num_factors <- length(specification@lhs)
-
   result <- standardizedsolution(specification) %>%
     filter(op == "=~") %>%
     mutate(item  = str_remove(rhs, name_items) %>% as.double(),
@@ -14,18 +12,18 @@ Standardized_solutions <- function(specification, name_items, apply_threshold = 
       names_from = lhs,
       values_from = c(est.std))
 
+  num_factors <- ncol(result) - 1
+
   if (apply_threshold) {
     result <- result %>%
       mutate(across(paste0("f", 1:num_factors), ~ case_when(
         . <= 0.30 ~ 0, TRUE ~ .)))
   }
 
-  factor_order <- paste0("desc(", paste0("f", 1:num_factors), ")", collapse = ", ")
-  factor_order <- parse_expr(paste("arrange(", factor_order, ")"))
+  factor_names <- paste0("f", 1:num_factors)
   result <- result %>%
-    eval_tidy(factor_order) %>%
+    arrange_at(vars(factor_names), .funs = list(desc)) %>%
     rename(Items = rhs)
 
   return(result)
 }
-
