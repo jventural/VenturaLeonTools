4f5d3e7731d40d8e724baac241cc501e77677537
jventural <jventuraleon@gmail.com>
2023-05-05 19:37:38 -0500

    change in extract items

--- R/extract_items.R
+++ R/extract_items.R
@@ -1,9 +1,13 @@
-extract_items <- function(text, prefix = "E") {
+extract_items <- function(text, prefix = "") {
   extracted <- list()
   for (line in text) {
     words <- strsplit(line, ": ")[[1]][-1]
     tag <- strsplit(line, ": ")[[1]][1]
-    values <- paste0(prefix, gsub(prefix, "", unlist(strsplit(words, ", "))))
+    values <- unlist(strsplit(words, ", "))
+
+    # Agregar prefijo solo a las letras mayúsculas seguidas de una o más letras minúsculas
+    values <- gsub("(?<=[A-Z])(?=[a-z])", paste0(prefix, ""), values, perl = TRUE)
+
     extracted[[tag]] <- values
   }
   return(extracted)
