4326c389d3d342689e0fa08ade1753b000ebee4e
jventural <jventuraleon@gmail.com>
2023-07-02 15:22:31 -0500

    Standard

--- R/Standardized_solutions.R
+++ R/Standardized_solutions.R
@@ -1,23 +1,30 @@
-Standardized_solutions <- function (specification, name_items, apply_threshold = TRUE)
-{
+Standardized_solutions <- function(specification, name_items, apply_threshold = TRUE) {
   library(tidyr)
   library(dplyr)
   library(stringr)
-  result <- standardizedsolution(specification) %>% filter(op ==
-                                                             "=~") %>% mutate(item = str_remove(rhs, name_items) %>%
-                                                                                as.double(), factor = str_remove(lhs, "f")) %>% select(lhs,
-                                                                                                                                       rhs, est.std) %>% pivot_wider(names_from = lhs, values_from = c(est.std))
 
-  # obtener los nombres de las columnas que comienzan con "f"
-  factor_names <- colnames(result)[startsWith(colnames(result), "f")]
+  result <- standardizedsolution(specification) %>%
+    filter(op == "=~") %>%
+    mutate(item  = str_remove(rhs, name_items) %>% as.double(),
+           factor = str_remove(lhs, "f")) %>%
+    select(lhs, rhs, est.std) %>%
+    pivot_wider(
+      names_from = lhs,
+      values_from = c(est.std))
+
+  num_factors <- ncol(result) - 1
 
   if (apply_threshold) {
-    result <- result %>% mutate(across(factor_names,
-                                       ~case_when(. <= 0.3 ~ 0, TRUE ~ .)))
+    result <- result %>%
+      mutate(across(paste0("f", 1:num_factors), ~ case_when(
+        . <= 0.30 ~ 0, TRUE ~ .)))
   }
 
-  result <- result %>% arrange_at(vars(factor_names), .funs = list(desc)) %>%
+  factor_names <- paste0("f", 1:num_factors)
+  result <- result %>%
+    arrange_at(vars(factor_names), .funs = list(desc)) %>%
     rename(Items = rhs)
+
   return(result)
 }
 
